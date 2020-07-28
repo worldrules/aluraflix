87e93d8b36f400fc6df7ca960450351332d96120
unknown <worldrulesdev@gmail.com>
2020-07-28 20:39:26 -0300

    eoq

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Menu from './components/menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <Menu></Menu>
+    <div style={{ background: '#141414' }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={'Curso de React em 1 hora'}
+      />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel category={dadosIniciais.categorias[1]} />
+
+      <Carousel category={dadosIniciais.categorias[2]} />
+
+      <Carousel category={dadosIniciais.categorias[3]} />
+
+      <Carousel category={dadosIniciais.categorias[4]} />
+
+      <Carousel category={dadosIniciais.categorias[5]} />
+
+      <Footer />
     </div>
   );
 }
